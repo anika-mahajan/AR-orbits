52597447e43c8dc34084330fb99f5f1d37034d8f
akinam2001 <anikamahajan.usa@gmail.com>
2025-07-15 17:31:26 -0600

    updated connection buttons

--- Assets/_orbit-sim/Scripts/Toio/MultiToioManager.cs
+++ Assets/_orbit-sim/Scripts/Toio/MultiToioManager.cs
@@ -18,6 +18,12 @@ public class MultiToioManager : MonoBehaviour
 
     bool _originLocked = true;
 
+    bool connectingSun = false;
+
+    bool connectingOrbiter = false;
+
+    bool connectingFocus = false;
+
     /// <summary>
     /// Text to display eccentricity
     /// </summary>
@@ -52,7 +58,6 @@ public class MultiToioManager : MonoBehaviour
     private GameObject playCheckmark;
 
     ButtonHandler OriginLockButton;
-    // ButtonHandler OriginLock;
 
     [SerializeField] 
     private GameObject lockBackground;
@@ -60,15 +65,44 @@ public class MultiToioManager : MonoBehaviour
     [SerializeField] 
     private GameObject lockCheckmark;
 
+
+    ButtonHandler SunButton;
+
+    [SerializeField] 
+    private GameObject sunBackground;
+    
+    [SerializeField] 
+    private GameObject sunCheckmark;
+
+    ButtonHandler OrbiterButton;
+
+    [SerializeField] 
+    private GameObject orbiterBackground;
+    
+    [SerializeField] 
+    private GameObject orbiterCheckmark;
+
+    ButtonHandler FocusButton;
+    // ButtonHandler OriginLock;
+
+    [SerializeField] 
+    private GameObject focusBackground;
+    
+    [SerializeField] 
+    private GameObject focusCheckmark;
+
     [SerializeField] 
     private GameObject debugCube;
     
     Cube cube;
 
+    int index;
+
 
     // Async Start to allow for Toio connect await
-    async void Start()
+    void Start()
     {
+        index = 0;
         // Create a CubeManager and connect to the closest Toio
         cubeManager = new CubeManager();
         // await cubeManager.MultiConnect(3);
@@ -77,31 +111,40 @@ public class MultiToioManager : MonoBehaviour
         // sunCube = cubeManager.cubes[1];
         // focusCube = cubeManager.cubes[2];
 
-        cube = await cubeManager.SingleConnect();
-        orbiterCube = cube;
-        cube = await cubeManager.SingleConnect();
-        sunCube = cube;
-        cube = await cubeManager.SingleConnect();
-        focusCube = cube;
+        // cube = await cubeManager.SingleConnect();
+        // orbiterCube = cube;
+        // cube = await cubeManager.SingleConnect();
+        // sunCube = cube;
+        // cube = await cubeManager.SingleConnect();
+        // focusCube = cube;
 
         // order is sun, focus, orbit (for connecting probably)
 
-        orbiterHandle = cubeManager.handles[0];
-        sunHandle = cubeManager.handles[1];
-        focusHandle = cubeManager.handles[2];
+        // orbiterHandle = cubeManager.handles[0];
+        // sunHandle = cubeManager.handles[1];
+        // focusHandle = cubeManager.handles[2];
 
         // Set behavior flags to default
         _shouldPlay = false;
 
         orbit = new Orbit();
-        orbit.setPositions(orbiterCube.x, orbiterCube.y, sunCube.x, sunCube.y, focusCube.x, focusCube.y);
-        orbit.calculateEllipse();
+        // orbit.setPositions(orbiterCube.x, orbiterCube.y, sunCube.x, sunCube.y, focusCube.x, focusCube.y);
+        // orbit.calculateEllipse();
 
         PlayButton = new ButtonHandler();
         PlayButton.setProperties(playBackground, playCheckmark, false);
 
         OriginLockButton = new ButtonHandler();
         OriginLockButton.setProperties(lockBackground, lockCheckmark, true);
+
+        SunButton = new ButtonHandler();
+        SunButton.setProperties(sunBackground, sunCheckmark, false);
+
+        OrbiterButton = new ButtonHandler();
+        OrbiterButton.setProperties(orbiterBackground, orbiterCheckmark, false);
+
+        FocusButton = new ButtonHandler();
+        FocusButton.setProperties(focusBackground, focusCheckmark, false);
     }
 
     void FixedUpdate()
@@ -135,6 +178,48 @@ public class MultiToioManager : MonoBehaviour
     {
         _shouldPlay = state;
         PlayButton.changeObj(_shouldPlay);
+    }
+
+    /// <summary>
+    /// Method to toggle play state
+    /// </summary>
+    /// <param name="state"></param>
+    public async void ToggleConnectingSun(bool state)
+    {
+        connectingSun = state;
+        cube = await cubeManager.SingleConnect();
+        sunCube = cube;
+        sunHandle = cubeManager.handles[index];
+        index++;
+        SunButton.changeObj(sunCube!=null);
+    }
+
+    /// <summary>
+    /// Method to toggle play state
+    /// </summary>
+    /// <param name="state"></param>
+    public async void ToggleConnectingOrbiter(bool state)
+    {
+        connectingOrbiter = state;
+        cube = await cubeManager.SingleConnect();
+        orbiterCube = cube;
+        orbiterHandle = cubeManager.handles[index];
+        index++;
+        OrbiterButton.changeObj(orbiterCube!=null);
+    }
+
+    /// <summary>
+    /// Method to toggle play state
+    /// </summary>
+    /// <param name="state"></param>
+    public async void ToggleConnectingFocus(bool state)
+    {
+        connectingFocus = state;
+        cube = await cubeManager.SingleConnect();
+        focusCube = cube;
+        focusHandle = cubeManager.handles[index];
+        index++;
+        FocusButton.changeObj(focusCube!=null);
     }
 
     /// <summary>
